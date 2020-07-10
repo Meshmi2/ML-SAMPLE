563d0bf20aaaa3e7cc6e3efb7d7c5354a46d6aa8
massimo trovati <massimotrovati92@gmail.com>
2020-07-10 12:10:34 +0200

    A-Machine-Learning-Sample
    simple machine learning sample...

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ML_SampleML.Model;
+
+namespace ML_Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Add input data
+            var input = new ModelInput();
+
+            // Load model and predict output of sample data
+            ModelOutput result = ConsumeModel.Predict(input);
+        }
+    }
+}
